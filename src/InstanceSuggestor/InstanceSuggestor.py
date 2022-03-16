8a48be148dab7c7d81dc3b583231858f6c6b78bc
Shruti Sharma <shruti.ss48@gmail.com>
2022-03-16 13:08:48 +0530

    Changed ReadMe

--- src/InstanceSuggestor/InstanceSuggestor.py
+++ src/InstanceSuggestor/InstanceSuggestor.py
@@ -5,14 +5,21 @@ import pandas as pd
 import numpy as np
 
 URL = "https://raw.githubusercontent.com/NetBook-ai/Instance-suggestor/main/data/benchmarks.csv"
-df = pd.read_csv(URL)
-df[['TimetoTrain_mins_', 'Cost To Train']] = df[['TimetoTrain_mins_', 'Cost To Train']].apply(pd.to_numeric, errors= 'coerce')
 
-def get_similar_instances(df=df, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
+def initialise(url):
+	"""
+	Utility Function
+
+	"""
+	df = pd.read_csv(url)
+	df[['TimetoTrain_mins_', 'Cost To Train']] = df[['TimetoTrain_mins_', 'Cost To Train']].apply(pd.to_numeric, errors= 'coerce')
+	return df
+
+def get_similar_instances(url=URL, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
 	"""
 	Get similar instance benchmarks based on framework, network and dataset
 
-	:param df: Source data
+	:param url: Source data
 	:param framework: MXNet, PyTorch, Tensorflow etc
 	:param network: Architecture/Network name
 	:param dataset: Name of dataset to run your network on
@@ -20,15 +27,15 @@ def get_similar_instances(df=df, framework='Tensorflow', network='ResNet-50 v1.5
 	:return: dataframe
 
 	"""
-	
+	df = initialise(url)
 	df3 = df[(df['Framework']==framework) & (df['Network'] == network) & (df['Dataset'] == dataset)]
 	return df3.iloc[:, :-1]
 
-def get_fastest_instance(df=df, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
+def get_fastest_instance(url=URL, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
 	'''
 	Get fastest instance based on framework, network and dataset
 
-	:param df: Source data
+	:param url: Source data
 	:param framework: MXNet, PyTorch, Tensorflow etc
 	:param network: Architecture/Network name
 	:param dataset: Name of dataset to run your network on
@@ -36,16 +43,17 @@ def get_fastest_instance(df=df, framework='Tensorflow', network='ResNet-50 v1.5'
 	:return: dataframe
 
 	'''
+	df = initialise(url)
 	df3 = df[(df['Framework']==framework) & (df['Network'] == network) & (df['Dataset'] == dataset)]
 	fi = df3['TimetoTrain_mins_'].min()
 	gpu_inf = df3.loc[df3['TimetoTrain_mins_'].isin([fi])]
 	return gpu_inf.iloc[:, :-1]
 
-def get_cheapest_instance(df=df, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
+def get_cheapest_instance(url=URL, framework='Tensorflow', network='ResNet-50 v1.5', dataset='ImageNet2012'):
 	'''
 	Get cheapest instance based on framework, network and dataset
 	
-	:param df: Source data
+	:param url: Source data
 	:param framework: MXNet, PyTorch, Tensorflow etc
 	:param network: Architecture/Network name
 	:param dataset: Name of dataset to run your network on
@@ -53,6 +61,7 @@ def get_cheapest_instance(df=df, framework='Tensorflow', network='ResNet-50 v1.5
 	:return: dataframe
 
 	'''
+	df = initialise(url)
 	df3 = df[(df['Framework']==framework) & (df['Network'] == network) & (df['Dataset'] == dataset)]
 	fi = df3['Cost To Train'].min()
 	gpu_inf = df3.loc[df3['Cost To Train'].isin([fi])]
--- src/InstanceSuggestor/suggest.py
+++ src/InstanceSuggestor/suggest.py
@@ -5,46 +5,45 @@ import pandas as pd
 import numpy as np
 
 URL = "https://raw.githubusercontent.com/NetBook-ai/Instance-suggestor/main/data/pricing.csv"
-df = pd.read_csv(URL)
 
-def input_data(df, instance):
+def input_data(url = URL, instance='p3.16xlarge'):
 	"""
 	utility function
 	
-	:param df: source file
+	:param url: source file
 	:param instance: Instance name
 
-	:return: tuple | (GPU Type, GPU Memory)
+	:return: tuple | (Source Data, GPU Type, GPU Memory)
 
 	"""
-
+	df = pd.read_csv(URL)
 	gpu_type = str(df['GPU Type'].values[df['Instance Name']==instance][0])
 	gpu_memory = df['GPU memory'].values[df['Instance Name']==instance][0]
-	return (gpu_type, gpu_memory)
+	return (df, gpu_type, gpu_memory)
 
-def predictor_sameRegion(df=df, instance='p3.16xlarge', region='Price_US_East1'):
+def predictor_sameRegion(url=URL, instance='p3.16xlarge', region='Price_US_East1'):
 	"""
 	Get Alternate Instances across different cloud providers same region of same GPU Type and same GPU Memory
 
-	:param df: Source file
+	:param url: Source file
 	:param instance: Instance name of any cloud
 	:param region: Region name in format - 'Price_Region_Area'
 
 	:return: dataframe
 	
 	"""
-	gpu_type, gpu_memory = input_data(df, instance)
+	df, gpu_type, gpu_memory = input_data(url, instance)
 	df2 = df[(df['GPU Type'] == gpu_type) & (df['GPU memory'] == gpu_memory)]
 	cols = df.columns[:8].values
 	np.append(cols, region)
 	return df2[cols]
 
 
-def predictor_sameRegion2(df=df, instance='p3.16xlarge', provider='AWS', region='Price_US_East1'):
+def predictor_sameRegion2(url=URL, instance='p3.16xlarge', provider='AWS', region='Price_US_East1'):
 	"""
 	Get Alternate Cheapest Instance on the same provider, same region of same GPU Type and same GPU Memory
 
-	:param df: Source file
+	:param url: Source file
 	:param instance: Instance name of a cloud provider
 	:param provider: Provider name - AWS, GCP, Azure
 	:param region: Region name in format - 'Price_Region_Area'
@@ -52,7 +51,7 @@ def predictor_sameRegion2(df=df, instance='p3.16xlarge', provider='AWS', region=
 	:return: dataframe
 	
 	"""
-	gpu_type, gpu_memory = input_data(df, instance)
+	df, gpu_type, gpu_memory = input_data(url, instance)
 	df2 = df[(df['GPU Type'] == gpu_type) & (df['GPU memory'] == gpu_memory)]
 	mp = df2[df2['Provider'] == provider]
 	cols = df.columns[:8].values
@@ -60,11 +59,11 @@ def predictor_sameRegion2(df=df, instance='p3.16xlarge', provider='AWS', region=
 	return mp[mp[region] == mp[region].min()][cols]
 
 
-def predictor_anyRegion(df=df, instance='p3.16xlarge', provider='AWS', region='Price_US_East1'):
+def predictor_anyRegion(url=URL, instance='p3.16xlarge', provider='AWS', region='Price_US_East1'):
 	"""
 	Get Alternate Cheapest Instance on the same provider, any region of same GPU Type and same GPU Memory
 
-	:param df: Source file
+	:param url: Source file
 	:param instance: Instance name of a cloud provider
 	:param provider: Provider name - AWS, GCP, Azure
 	:param region: Region name in format - 'Price_Region_Area'
@@ -72,33 +71,32 @@ def predictor_anyRegion(df=df, instance='p3.16xlarge', provider='AWS', region='P
 	:return: dataframe
 	
 	"""
-	gpu_type, gpu_memory = input_data(df, instance)
+	df, gpu_type, gpu_memory = input_data(url, instance)
 	df2 = df[(df['GPU Type'] == gpu_type) & (df['GPU memory'] == gpu_memory)]
 	cols1 = df.columns[7:]
 	mp = df2[df2['Provider'] == provider]
 	mpr = mp[cols1].min().min()
-	#print(mp[(mp[cols1[0]] == mpr) | (mp[cols1[1]] == mpr) | (mp[cols1[2]] == mpr) | (mp[cols1[3]] == mpr)].to_string(index = False))
 	return mp[(mp[cols1[0]] == mpr) | (mp[cols1[1]] == mpr) | (mp[cols1[2]] == mpr) | (mp[cols1[3]] == mpr)]
 
-def predictor_cheapest(df=df, instance='p3.16xlarge', region='Price_US_East1'):
+def predictor_cheapest(url=URL, instance='p3.16xlarge', region='Price_US_East1'):
 	"""
 	Get Cheapest Instance on any provider, same region of same GPU Type and same GPU Memory
 
-	:param df: Source file
+	:param url: Source file
 	:param instance: Instance name of a cloud provider
 	:param region: Region name in format - 'Price_Region_Area'
 
 	:return: dataframe
 	
 	"""
-	gpu_type, gpu_memory = input_data(df, instance)
+	df, gpu_type, gpu_memory = input_data(url, instance)
 	df2 = df[(df['GPU Type'] == gpu_type) & (df['GPU memory'] == gpu_memory)]
 	cols = df.columns[:8].values
 	np.append(cols, region)
 	return df2[df2[region] == df2[region].min()][cols]
 
 
-def predictor_cheapest2(df=df, instance='p3.16xlarge'):
+def predictor_cheapest2(url=URL, instance='p3.16xlarge'):
 	"""
 	Get Cheapest Instance on any provider, any region of same GPU Type and same GPU Memory
 
@@ -108,14 +106,9 @@ def predictor_cheapest2(df=df, instance='p3.16xlarge'):
 	:return: dataframe
 	
 	"""
-	gpu_type, gpu_memory = input_data(df, instance)
+	df, gpu_type, gpu_memory = input_data(url, instance)
 	df2 = df[(df['GPU Type'] == gpu_type) & (df['GPU memory'] == gpu_memory)]
 	cols1 = df.columns[7:]
 	mpr = df2[cols1].min().min()
 	return df2[(df2[cols1[0]] == mpr) | (df2[cols1[1]] == mpr) | (df2[cols1[2]] == mpr) | (df2[cols1[3]] == mpr)]
-
-
-
-
-
-
+	
